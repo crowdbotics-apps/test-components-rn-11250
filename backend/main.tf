3b745c223aaa52a8bad7aec8916a21868de7675c
Crowdbotics <team@crowdbotics.com>
2019-12-11 16:33:05 +0000

    Initial commit.

---
+++ backend/main.tf
@@ -0,0 +1,14 @@
+provider "heroku" {
+  version = "~> 2.2.1"
+}
+provider "cloudflare" {
+  version = "~> 1.9"
+}
+terraform {
+  backend "s3" {
+    bucket  = "crowdbotics-terraform"
+    key     = "test_components_rn_11250"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
